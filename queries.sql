3a1b70cd249fdc064be3f9b756c5610b27280cea
Jessica <jrnewport07@gmail.com>
2020-03-15 16:54:12 -0500

    assignment 4 complete

--- queries.sql
+++ queries.sql
@@ -1,4 +1,7 @@
 ## Part 1: Test it with SQL
+
+SHOW columns FROM job;
+
 id
 int PK
 employer
@@ -7,6 +10,7 @@ name
 varchar(255)
 skills
 varchar(255)
+
 ## Part 2: Test it with SQL
 
 SELECT name FROM employer
@@ -18,3 +22,8 @@ DROP TABLE job;
 
 ## Part 4: Test it with SQL
 
+SELECT name, description
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE jobs_id IS NOT NULL
+ORDER BY name ASC;
